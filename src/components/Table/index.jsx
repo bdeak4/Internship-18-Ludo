858f7dc20bfeabf7ad0814007a6a08964bb6448b
Bartol Deak <b@bdeak.net>
2022-03-22 21:27:43 +0100

    add simple board grid

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,14 +1,18 @@
 import React, { useContext } from "react";
 import { GameContext } from "../../providers/game";
+import { TableElement } from "./styled";
 
 const Table = () => {
   const { game } = useContext(GameContext);
 
   return (
-    <div>
-      table
-      <pre>{JSON.stringify(game)}</pre>
-    </div>
+    <TableElement>
+      <div className="player-home ph0">ph0</div>
+      <div className="player-home ph1">ph1</div>
+      <div className="player-home ph2">ph2</div>
+      <div className="player-home ph3">ph3</div>
+      <div className="center">center</div>
+    </TableElement>
   );
 };
 
